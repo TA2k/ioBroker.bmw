903351ecb67bce1b718af12c2010dcd1b4b15e93
TA2k <tombox2020@gmail.com>
2021-08-02 23:22:34 +0200

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "bmw adapter settings": {
+        en: "Adapter settings for bmw",
+        de: "Adaptereinstellungen für bmw",
+        ru: "Настройки адаптера для bmw",
+        pt: "Configurações do adaptador para bmw",
+        nl: "Adapterinstellingen voor bmw",
+        fr: "Paramètres d'adaptateur pour bmw",
+        it: "Impostazioni dell'adattatore per bmw",
+        es: "Ajustes del adaptador para bmw",
+        pl: "Ustawienia adaptera dla bmw",
+        "zh-cn": "bmw2的适配器设置",
+    },
+    option1: {
+        en: "option1",
+        de: "Option 1",
+        ru: "Опция 1",
+        pt: "Opção 1",
+        nl: "Optie 1",
+        fr: "Option 1",
+        it: "opzione 1",
+        es: "Opción 1",
+        pl: "opcja 1",
+        "zh-cn": "选项1",
+    },
+    option2: {
+        en: "option2",
+        de: "Option 2",
+        ru: "option2",
+        pt: "opção 2",
+        nl: "Optie 2",
+        fr: "Option 2",
+        it: "opzione 2",
+        es: "opcion 2",
+        pl: "Opcja 2",
+        "zh-cn": "选项2",
+    },
+};
---
+++ main.js
@@ -0,0 +1,311 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v1.34.1
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+const axios = require("axios");
+const qs = require("qs");
+const { extractKeys } = require("./lib/extractKeys");
+
+class Bmw extends utils.Adapter {
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: "bmw",
+        });
+        this.on("ready", this.onReady.bind(this));
+        this.on("stateChange", this.onStateChange.bind(this));
+        this.on("unload", this.onUnload.bind(this));
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        // Reset the connection indicator during startup
+        this.setState("info.connection", false, true);
+        if (this.config.interval < 0.5) {
+            this.log.info("Set interval to minimum 0.5");
+            this.config.interval = 0.5;
+        }
+        this.requestClient = axios.create();
+        this.updateInterval = null;
+        this.extractKeys = extractKeys;
+        this.vinArray = [];
+        this.session = {};
+        this.canGen = {};
+        this.subscribeStates("*");
+
+        await this.login();
+        if (this.session.access_token) {
+            await this.getVehicles();
+            await this.updateVehicles();
+            this.updateInterval = setInterval(async () => {
+                await this.updateVehicles();
+            }, this.config.interval * 60 * 1000);
+            this.refreshTokenInterval = setInterval(() => {
+                this.refreshToken();
+            }, this.session.expires_in * 1000);
+        }
+    }
+    async login() {
+        const headers = {
+            Accept: "application/json, text/plain, */*",
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1",
+            "Accept-Language": "de-de",
+            "Content-Type": "application/x-www-form-urlencoded",
+        };
+        const data = qs.stringify({
+            client_id: "31c357a0-7a1d-4590-aa99-33b97244d048",
+            response_type: "code",
+            scope: "openid profile email offline_access smacc vehicle_data perseus dlm svds cesim vsapi remote_services fupo authenticate_user",
+            redirect_uri: "com.bmw.connected://oauth",
+            state: "cEG9eLAIi6Nv-aaCAniziE_B6FPoobva3qr5gukilYw",
+            nonce: "login_nonce",
+            username: this.config.username,
+            password: this.config.password,
+            grant_type: "authorization_code",
+        });
+        const authUrl = await this.requestClient({
+            method: "post",
+            url: "https://customer.bmwgroup.com/gcdm/oauth/authenticate",
+            headers: headers,
+            data: data,
+        })
+            .then((res) => {
+                this.log.debug(JSON.stringify(res.data));
+                return res.data;
+            })
+            .catch((error) => {
+                this.log.error(error);
+            });
+        if (!authUrl.redirect_to) {
+            this.log.error(JSON.stringify(authUrl));
+            return;
+        }
+        const code = await this.requestClient({
+            method: "get",
+            url: authUrl.redirect_to,
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            },
+        })
+            .then((res) => {
+                this.log.debug(JSON.stringify(res.data));
+                return res.data;
+            })
+            .catch((error) => {
+                let code = "";
+                if (error.config) {
+                    this.log.debug(JSON.stringify(error.config.url));
+                    const qArray = error.config.url.split("?")[1].split("&");
+                    qArray.forEach((element) => {
+                        const elementArray = element.split("=");
+                        if (elementArray[0] === "code") {
+                            code = elementArray[1];
+                        }
+                    });
+                    this.log.debug(code);
+                }
+                return code;
+            });
+        await this.requestClient({
+            method: "post",
+            url: "https://customer.bmwgroup.com/gcdm/oauth/token",
+
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+                Accept: "*/*",
+            },
+            data: "code=" + code + "&code_verifier=7PsmfPS5MpaNt0jEcPpi-B7M7u0gs1Nzw6ex0Y9pa-0&redirect_uri=com.bmw.connected://oauth&grant_type=authorization_code",
+        })
+            .then((res) => {
+                this.log.debug(JSON.stringify(res.data));
+                this.session = res.data;
+                this.setState("info.connection", true, true);
+                return res.data;
+            })
+            .catch((error) => {
+                this.log.error(error);
+            });
+    }
+    async getVehicles() {
+        const headers = {
+            "Content-Type": "application/json",
+            Accept: "*/*",
+            Authorization: "Bearer " + this.session.access_token,
+        };
+
+        await this.requestClient({
+            method: "get",
+            url: "https://b2vapi.bmwgroup.com/webapi/v1/user/vehicles",
+            headers: headers,
+        })
+            .then(async (res) => {
+                this.log.debug(JSON.stringify(res.data));
+                for (const vehicle of res.data.data) {
+                    this.vinArray.push(vehicle.vin);
+                    await this.setObjectNotExistsAsync(vehicle.vin, {
+                        type: "device",
+                        common: {
+                            name: vehicle.model,
+                            role: "state",
+                        },
+                        native: {},
+                    });
+                    await this.setObjectNotExistsAsync(vehicle.vin + ".remote", {
+                        type: "channel",
+                        common: {
+                            name: "Remote Controls",
+                            role: "state",
+                        },
+                        native: {},
+                    });
+                    const remoteArray = [{ command: "lock-unlock" }];
+                    remoteArray.forEach((remote) => {
+                        this.setObjectNotExists(vehicle.vin + ".remote." + remote.command, {
+                            type: "state",
+                            common: {
+                                name: remote.name || "",
+                                type: remote.type || "boolean",
+                                role: remote.role || "boolean",
+                                write: true,
+                                read: true,
+                            },
+                            native: {},
+                        });
+                    });
+                    this.extractKeys(this, vehicle.vin + ".general", vehicle);
+                }
+            })
+            .catch((error) => {
+                this.log.error(error);
+            });
+    }
+    async updateVehicles() {
+        const date = new Date();
+        let month = date.getMonth() + 1;
+        month = (month > 9 ? "" : "0") + month;
+        const yyyymmm = date.getFullYear() + "" + month;
+        const statusArray = [{ path: "status", url: "https://b2vapi.bmwgroup.com/webapi/v1/user/vehicles/$vin/status" }];
+        const headers = {
+            "Content-Type": "application/vnd.api+json",
+            Accept: "*/*",
+
+            Authorization: "Bearer " + this.session.access_token,
+        };
+        this.vinArray.forEach((vin) => {
+            statusArray.forEach(async (element) => {
+                const url = element.url.replace("$vin", vin);
+                await this.requestClient({
+                    method: "get",
+                    url: url,
+                    headers: headers,
+                })
+                    .then((res) => {
+                        this.log.debug(JSON.stringify(res.data));
+                        let data = res.data;
+                        if (data.data) {
+                            data = data.data;
+                        }
+                        if (data.attributes) {
+                            data = data.attributes;
+                        }
+                        const forceIndex = null;
+                        const preferedArrayName = null;
+
+                        this.extractKeys(this, vin + "." + element.path, data, preferedArrayName, forceIndex);
+                    })
+                    .catch((error) => {
+                        if (error.response.status !== 502) {
+                            this.log.error(error);
+                            error.response && this.log.error(JSON.stringify(error.response.data));
+                        }
+                    });
+            });
+        });
+    }
+    async refreshToken() {
+        await this.requestClient({
+            method: "post",
+            url: "https://customer.bmwgroup.com/gcdm/oauth/token",
+            jar: this.cookieJar,
+            withCredentials: true,
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+                Accept: "application/json",
+            },
+            data: "client_id=31c357a0-7a1d-4590-aa99-33b97244d048&grant_type=refresh_token&refresh_token=" + this.session.refresh_token,
+        })
+            .then((res) => {
+                this.log.debug(JSON.stringify(res.data));
+                this.session.access_token = res.data.access_token;
+                this.setState("info.connection", true, true);
+                return res.data;
+            })
+            .catch((error) => {
+                this.log.error(error);
+            });
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            clearTimeout(this.refreshTimeout);
+
+            clearInterval(this.updateInterval);
+            clearInterval(this.refreshTokenInterval);
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    onStateChange(id, state) {
+        if (state) {
+            if (!state.ack) {
+                const vin = id.split(".")[2];
+                const command = id.split(".")[4];
+            } else {
+                const resultDict = {  };
+                const idArray = id.split(".");
+                const stateName = idArray[idArray.length - 1];
+
+                if (resultDict[stateName]) {
+                    const vin = id.split(".")[2];
+                    let value = true;
+                    if (!state.val || state.val === "off" || state.val === "unlocked") {
+                        value = false;
+                    }
+                    await this.setStateAsync(vin + ".remote." + resultDict[stateName], value, true);
+                }
+            }
+    }
+}
+
+if (require.main !== module) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new Bmw(options);
+} else {
+    // otherwise start the instance directly
+    new Bmw();
+}
